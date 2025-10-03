8bbfa56e692a1afefd124478880794d5b80ee083
Pranesh <praneshmadhan646@gmail.com>
2025-10-03 21:06:44 +0530

    added some builtins
    Co-authored-by: Pranesh S <praneshpalamedu17@gmail.com>

--- builtins/builtins.go
+++ builtins/builtins.go
@@ -9,4 +9,9 @@ func main(){
 	fmt.Println("Round of 52.64 is ", round)
 	cbrt := math.Cbrt(27)
 	fmt.Println("Cube root of 27 is ", cbrt)
+	mx := math.Max(5,2)
+	fmt.Println("Max of 5 and 2 is ", mx)
+	min := math.Min(6,4)
+	fmt.Println("Min of 6 and 4 is ", min)
+
 }
